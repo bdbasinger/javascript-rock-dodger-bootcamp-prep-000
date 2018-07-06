d421c4dbb1da6675bb19df6a4afdc1bb1c2f0dfa
Brennan B <bdbasinger777@gmail.com>
2018-07-06 16:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -207,11 +207,21 @@ function positionToInteger(p) {
 
 function start() {
   window.addEventListener('keydown', moveDodger)
-
   START.style.display = 'none'
   
-
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
   }, 2000)
 }
+
+
+
+
+
+
+
+
+
+
+
+
