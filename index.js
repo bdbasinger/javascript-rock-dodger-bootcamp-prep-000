16865cdfd2d1bf504ba57d0d83509213865713ec
Brennan B <bdbasinger777@gmail.com>
2018-07-06 16:10:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,17 +43,15 @@ function checkCollision(rock) {
   
   if (top > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
-    // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
+    
     const dodgerRightEdge = dodgerLeftEdge + 40;
     
     const rockLeftEdge = positionToInteger(rock.style.left)
-    // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
+    
     const rockRightEdge = rockLeftEdge + 20;
 
-    if (false ) {
-      
-      return true
-    }
+      return (
+        )
   }
 }
 
